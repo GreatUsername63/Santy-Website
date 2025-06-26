f5f5db11b6dd79908bd546c3773f6ab01b46b9fe
GreatUsername63 <RamirezSantiago244@gmail.com>
2025-06-25 21:16:44 -0700

    project page

--- src/Pages/index.jsx
+++ src/Pages/index.jsx
@@ -1,4 +1,6 @@
-import autoretrato from './assets/08_05_24 autoretrato chistoso 1.png'
+import autoretrato from '../assets/08_05_24 autoretrato chistoso 1.png'
+import "../styles/index.css"
+import { Link } from "react-router-dom"
 
 function App() {
   return (
@@ -7,6 +9,11 @@ function App() {
         <h1>Hi!</h1>
         <p>My name is Santiago Ramírez Enríquez, a junior software engineer and aspirant video game programmer.
           I'm passionate for programming fun and interesting experiences that people can enjoy.</p>
+        <div id="button-container">
+          <Link class="index-button" id="pink-button" to={"/projects"}>Projects</Link>
+          <Link class="index-button" id="blue-button" to={"/education"}>Education</Link>
+        </div>
+
       </aside>
       <div id='main-image-container'>
         <img src={autoretrato}></img>
