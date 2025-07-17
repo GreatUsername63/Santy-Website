165e4b54cfe21be558aa44e9557e3e0794c5f00f
GreatUsername63 <RamirezSantiago244@gmail.com>
2025-07-17 16:32:08 -0700

    added react bootstrap icons

--- src/Pages/projects.jsx
+++ src/Pages/projects.jsx
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react"
+import * as Icons from 'react-bootstrap-icons'
 
 function App() {
     const [projects, setProjects] = useState([])
@@ -14,6 +15,7 @@ function App() {
         <>
             <aside id="project-aside">
                 <div id="project-aside-header-container">
+                    <Icons.ArrowLeft size={50} />
                     <h2>Projects</h2>
                 </div>
                 <div id="project-aside-button-container">
