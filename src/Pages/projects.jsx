d7e2faaff93f19838097a5648c3e72dc3c18bf56
GreatUsername63 <RamirezSantiago244@gmail.com>
2025-07-17 18:27:16 -0700

    projects main area css

--- src/Pages/projects.jsx
+++ src/Pages/projects.jsx
@@ -2,6 +2,8 @@ import { useEffect, useState } from "react"
 import styles from '../styles/projects.module.css'
 import * as Icons from 'react-bootstrap-icons'
 
+import placeholderImage from '../assets/imagen 1.png'
+
 function App() {
     const [projects, setProjects] = useState([])
 
@@ -30,6 +32,18 @@ function App() {
                     }
                 </div>
             </aside>
+            <div className={styles.mainContainer}>
+                <h1>2D DODGER</h1>
+                <p>Solo Lua developer</p>
+                <img src={placeholderImage}></img>
+                <ul>
+                    <li>Simple game about avoiding rocks using two spaceships placed in the vertical and horizontal axis. Done in approximately one day.</li>
+                    <li>Used object oriented programming in a limited system with dictionaries and collision algorithms.</li>
+                    <li>Used Lua and Pico-8.</li>
+                </ul>
+                <a href="google.com">[[[Game Link]]]</a>
+                <a href="google.com">[[[Github Link]]]</a>
+            </div>
         </>
     )
 }
